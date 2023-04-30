9a71eb9c01d4ac020edc2df9ca6520fcc8004e9f
GE28 <44209041+GE28@users.noreply.github.com>
2023-04-30 12:37:23 -0300

    refactor: remove .env checking from conn

--- src/db/conn.mjs
+++ src/db/conn.mjs
@@ -1,20 +1,11 @@
 import chalk from 'chalk';
 import mongoose from 'mongoose';
 
-const uri = process.env.MONGODB_URI;
-if (!uri) {
-  console.error(chalk.red.bold('Variável de ambiente MONGODB_URI não definida!'));
-  process.exit(1);
-} 
-
-let connection;
-
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log(chalk.green('Conectado ao'), chalk.green.bold('MongoDB Atlas!'));
-    connection = mongoose.connection;
-    console.log(chalk.yellow('Banco de dados:'), connection.db.databaseName);
-    console.log(chalk.yellow('Host:'), connection.host);
-    console.log(chalk.yellow('Porta:'), connection.port);
+    console.log(chalk.yellow('Banco de dados:'), mongoose.connection.db.databaseName);
+    console.log(chalk.yellow('Host:'), mongoose.connection.host);
+    console.log(chalk.yellow('Porta:'), mongoose.connection.port);
   })
   .catch(err => console.error(chalk.red.bold('Erro ao conectar ao MongoDB Atlas:'), err));
