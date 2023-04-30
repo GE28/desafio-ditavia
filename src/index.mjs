93ed29b1619fd2f2c3249c4845554f0b4d50390a
GE28 <44209041+GE28@users.noreply.github.com>
2023-04-30 12:36:47 -0300

    feat: add .env vars checking

--- src/index.mjs
+++ src/index.mjs
@@ -1,9 +1,19 @@
 import './loadEnvironment.mjs';
-import { app } from './app.mjs';
+import { server } from './server.mjs';
 import chalk from 'chalk';
 
-const port = process.env.PORT || 3000;
+const port = process.env.SERVER_PORT;
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Express ${chalk.green('iniciado')} ${chalk.blue.bold(`(http://localhost:${port})`)}`);
 });
+
+if (!process.env.MONGODB_URI) {
+  console.error(chalk.red.bold('Variável de ambiente MONGODB_URI não definida!'));
+  process.exit(1);
+} 
+
+if (!process.env.SERVER_PORT) {
+  console.error(chalk.red.bold('Variável de ambiente SERVER_PORT não definida!'));
+  process.exit(1);
+}
--- src/loadEnvironment.mjs
+++ src/loadEnvironment.mjs
@@ -1,7 +1,7 @@
 import * as dotenv from 'dotenv';
 import path from 'path';
-
 import { fileURLToPath } from 'url';
+
 const __dirname = fileURLToPath(import.meta.url);
 
 dotenv.config({ path: path.join(__dirname, '..', '..', '.env') });
