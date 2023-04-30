396687863b7bd25355779852b88557b369112281
GE28 <44209041+GE28@users.noreply.github.com>
2023-04-30 12:46:13 -0300

    feat: open the home page
    As soon as the app starts

--- src/index.mjs
+++ src/index.mjs
@@ -1,12 +1,9 @@
 import './loadEnvironment.mjs';
 import { server } from './server.mjs';
 import chalk from 'chalk';
+import open from 'open';
 
 const port = process.env.SERVER_PORT;
-
-server.listen(port, () => {
-  console.log(`Express ${chalk.green('iniciado')} ${chalk.blue.bold(`(http://localhost:${port})`)}`);
-});
 
 if (!process.env.MONGODB_URI) {
   console.error(chalk.red.bold('Variável de ambiente MONGODB_URI não definida!'));
@@ -17,3 +14,11 @@ if (!process.env.SERVER_PORT) {
   console.error(chalk.red.bold('Variável de ambiente SERVER_PORT não definida!'));
   process.exit(1);
 }
+
+server.listen(port, () => {
+  console.log(`Express ${chalk.green('iniciado')} ${chalk.blue.bold(`(http://localhost:${port})`)}`);
+  console.log('A página será aberta automaticamente em 2 segundos...');
+  setTimeout(() => {
+    open(`http://localhost:${port}`);
+  }, 2000);
+});
