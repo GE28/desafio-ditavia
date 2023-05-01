03bf7eb2ff2692154bcfea976a32aa5658682f62
GE28 <44209041+GE28@users.noreply.github.com>
2023-05-01 18:45:24 -0300

    feat: add routes

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import './index.css';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import Home from './pages/Home';
+import CreateSmartphoneForm from './pages/CreateSmartphoneForm';
+import UpdateSmartphoneForm from './pages/UpdateSmartphoneForm';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: < Home />
+  }, {
+    path: '/edit-smartphones/:id',
+    element: <UpdateSmartphoneForm />
+  },
+  {
+    path: '/add-smartphones',
+    element: <CreateSmartphoneForm />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('app')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
