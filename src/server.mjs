abbe298cce79e9951631fd59cab12e00af0701c9
GE28 <44209041+GE28@users.noreply.github.com>
2023-05-01 18:50:48 -0300

    fix: add cors to allow front end access

--- src/server.mjs
+++ src/server.mjs
@@ -2,9 +2,11 @@ import express from 'express';
 
 import './db/conn.mjs';
 import smartphonesRoutes from './routes/Smartphones.mjs';
+import cors from 'cors';
 
 const server = express();
 
+server.use(cors());
 server.use(express.json());
 
 server.use('/api/smartphone', smartphonesRoutes);
